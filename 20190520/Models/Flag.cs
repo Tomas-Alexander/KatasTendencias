bfd34c625a070e91870bab79bf1f0d4fc51e790c
Tomas_Alexander <el.carta@hotmail.com>
2019-05-27 19:39:44 -0400

    Create Flag.cs

---
+++ 20190520/Models/Flag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArgsKata1.Models
+{
+    class Flag
+    {
+        public char Name { get; set; }
+        public Type Type { get; set; }
+
+        public Flag(char name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
