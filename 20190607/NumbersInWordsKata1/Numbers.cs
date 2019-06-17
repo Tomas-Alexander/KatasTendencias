6c2bf8b42784de07f041c6ff8d055ef1ab2234de
Tomas_Alexander <el.carta@hotmail.com>
2019-06-17 15:04:31 -0400

    NumbersInWords

---
+++ 20190607/NumbersInWordsKata1/Numbers.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+
+namespace NumbersInWordsKata1
+{
+    public class Numbers
+    {
+        public static string ToWords(int v)
+        {
+            string result = "";
+            int number = v;
+            for (int i = v.ToString().Length; i > 0; i--)
+            {
+                if (v >= 20)
+                {
+                    if (i != 1)
+                    {
+                        number = v % Convert.ToInt32(Math.Pow(10d,i)) - v % Convert.ToInt32(Math.Pow(10d,i-1));
+                    }
+                    else
+                    {
+
+                        number = v % Convert.ToInt32(Math.Pow(10d, i));
+                    }
+                }
+                else
+                {
+                    i = 0;
+                }
+
+
+
+                switch (number)
+                {
+                    case 1:
+                        result += "One";
+                        break;
+                    case 2:
+                        result += "Two";
+                        break;
+                    case 3:
+                        result += "Three";
+                        break;
+                    case 4:
+                        result += "Four";
+                        break;
+                    case 5:
+                        result += "Five";
+                        break;
+                    case 6:
+                        result += "Six";
+                        break;
+                    case 7:
+                        result += "Seven";
+                        break;
+                    case 8:
+                        result += "Eigth";
+                        break;
+                    case 9:
+                        result += "Nine";
+                        break;
+                    case 10:
+                        result += "Ten";
+                        break;
+                    case 11:
+                        result += "Eleven";
+                        break;
+                    case 12:
+                        result += "Twelve";
+                        break;
+                    case 13:
+                        result += "Thirteen";
+                        break;
+                    case 14:
+                        result += "Fourteen";
+                        break;
+                    case 15:
+                        result += "Fifteen";
+                        break;
+                    case 16:
+                        result += "Sixteen";
+                        break;
+                    case 17:
+                        result += "Seventeen";
+                        break;
+                    case 18:
+                        result += "Eighteen";
+                        break;
+                    case 19:
+                        result += "Nineteen";
+                        break;
+                    case 20:
+                        result += "Twenty";
+                        break;
+                    default:
+                        result += "";
+                        break;
+                }
+                result += " ";
+            }
+            return result.Trim();
+
+        }
+    }
+}
---
+++ 20190607/NumbresInWordsKata1Tests/NumbersInWordsKata1Test.cs
@@ -0,0 +1,55 @@
+﻿using NumbersInWordsKata1;
+using NUnit.Framework;
+
+namespace NumbresInWordsKata1Tests
+{
+    [TestFixture]
+    public class NumbersInWordsKata1Test
+    {
+
+        [Test]
+        [TestCase(1, "One")]
+        [TestCase(2, "Two")]
+        [TestCase(3, "Three")]
+        [TestCase(4, "Four")]
+        [TestCase(5, "Five")]
+        [TestCase(6, "Six")]
+        [TestCase(7, "Seven")]
+        [TestCase(8, "Eigth")]
+        [TestCase(9, "Nine")]
+        public void FromNumberToWords_WithOneDigitNumber_ReturnCorrectString(int n, string expected)
+        {
+            string result = Numbers.ToWords(n);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(10, "Ten")]
+        [TestCase(11, "Eleven")]
+        [TestCase(12, "Twelve")]
+        [TestCase(13, "Thirteen")]
+        [TestCase(14, "Fourteen")]
+        [TestCase(16, "Sixteen")]
+        [TestCase(17, "Seventeen")]
+        [TestCase(18, "Eighteen")]
+        [TestCase(19, "Nineteen")]
+        public void FromNumberToWords_WithNumberFrom10To19_ReturnCorrectString(int n, string expected)
+        {
+            string result = Numbers.ToWords(n);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(20, "Twenty")]
+        [TestCase(21, "Twenty One")]
+        [TestCase(29, "Twenty Nine")]
+        public void FromNumberToWords_WithNumberFrom20To29_ReturnCorrectString(int n, string expected)
+        {
+            string result = Numbers.ToWords(n);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
