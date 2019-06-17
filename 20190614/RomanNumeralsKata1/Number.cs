edbf008f32df61ce4df02370723e860941382946
Tomas_Alexander <el.carta@hotmail.com>
2019-06-17 15:24:22 -0400

    RomanNumerals1

---
+++ 20190614/RomanNumeralsKata1/Number.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RomanNumeralsKata1
+{
+    public class Number
+    {
+        public static string ToRoman(int v)
+        {
+            string roman = "";
+            for (int i = v.ToString().Length; i > 0; i--)
+            {
+                int result = v % Convert.ToInt32(Math.Pow(10, i));
+                result = result - result % Convert.ToInt32(Math.Pow(10, i-1));
+
+                switch (result)
+                {
+                    case 1:
+                        roman += "I";
+                        break;
+                    case 2:
+                        roman += "II";
+                        break;
+                    case 3:
+                        roman += "III";
+                        break;
+                    case 4:
+                        roman += "IV";
+                        break;
+                    case 5:
+                        roman += "V";
+                        break;
+                    case 6:
+                        roman += "VI";
+                        break;
+                    case 7:
+                        roman += "VII";
+                        break;
+                    case 8:
+                        roman += "VIII";
+                        break;
+                    case 9:
+                        roman += "IX";
+                        break;
+                    case 10:
+                        roman += "X";
+                        break;
+                    case 20:
+                        roman += "XX";
+                        break;
+                    default:
+                        roman += "";
+                        break;
+                }
+            }
+
+            return roman;
+        }
+    }
+}
---
+++ 20190614/RomanNumeralsKata1Tests/RomanNumeralsKata1Test.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using RomanNumeralsKata1;
+
+namespace RomanNumeralsKata1Tests
+{
+    [TestFixture]
+    public class RomanNumeralsKata1Test
+    {
+        [Test]
+        [TestCase(1,"I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        public void FromNumberToRoman_WithNumbersFromOneToTen_ShoulReturnExpectedValue(int number, string expected)
+        {
+            string result = Number.ToRoman(number);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(13, "XIII")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(16, "XVI")]
+        [TestCase(17, "XVII")]
+        [TestCase(18, "XVIII")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        public void FromNumberToRoman_WithNumbersFromElevenToTwenty_ShouldReturnExpected(int number, string expected)
+        {
+            string result = Number.ToRoman(number);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void FromNumberToRoman_WithNumberTwelve_ShouldReturnXII()
+        {
+            string result = Number.ToRoman(12);
+            Assert.AreEqual("XII", result);
+        }
+    }
+}
