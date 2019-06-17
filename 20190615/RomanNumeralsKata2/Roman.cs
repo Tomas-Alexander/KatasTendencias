0c2c83aa3f43be3ef22487a5f2aa9fc26e8328dc
Tomas_Alexander <el.carta@hotmail.com>
2019-06-17 15:25:24 -0400

    RomanNumeralsKata2

---
+++ 20190615/RomanNumeralsKata2/Roman.cs
@@ -0,0 +1,40 @@
+﻿namespace RomanNumeralsKata2
+{
+    public class Roman
+    {
+        public static string Parse(int v)
+        {
+            int number = v
+            for (int i = number.ToString().Length; i > 0; i--)
+            {
+
+                switch (v)
+                {
+                    case 1:
+                        return "I";
+                    case 2:
+                        return "II";
+                    case 3:
+                        return "III";
+                    case 4:
+                        return "IV";
+                    case 5:
+                        return "V";
+                    case 6:
+                        return "VI";
+                    case 7:
+                        return "VII";
+                    case 8:
+                        return "VIII";
+                    case 9:
+                        return "IX";
+                    case 10:
+                        return "X";
+                    default:
+                        return "";
+                }
+            }
+            
+        }
+    }
+}
---
+++ 20190615/RomanNumeralsKata2Tests/RomanNumeralsKata2Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using RomanNumeralsKata2;
+
+namespace RomanNumeralsKata2Tests
+{
+    [TestFixture]
+    public class RomanNumeralsKata2Test
+    {
+        [Test]
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        public void FromNumberToRoman_WithNumberOne_ShoulReturnI(int number, string expected)
+        {
+            string result = Roman.Parse(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void FromNumberToRoman_WithNumberEleven_shouldReturnXI()
+        {
+            string result = Roman.Parse(11);
+
+            Assert.AreEqual("XI", result);
+        }
+    }
+}
